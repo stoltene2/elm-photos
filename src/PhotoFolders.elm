9ce95edc6497510c77b5ee8a4f19025ddad6a123
Eric Stolten <stoltene2@gmail.com>
2021-06-03 20:22:49 -0400

    Add ability to show related photos

--- src/PhotoFolders.elm
+++ src/PhotoFolders.elm
@@ -1,14 +1,27 @@
 module PhotoFolders exposing (main)
 
 import Browser
-import Html exposing (Html)
+import Dict exposing (Dict)
+import Html exposing (Html, div, h2, h3, img, span, text)
+import Html.Attributes exposing (class, src)
+import Html.Events exposing (onClick)
 import Http
 import Json.Decode as Decode exposing (Decoder, int, list, string)
 import Json.Decode.Pipeline exposing (required)
+import PhotoGroove exposing (urlPrefix)
+
+
+type alias Photo =
+    { title : String
+    , size : Int
+    , relatedUrls : List String
+    , url : String
+    }
 
 
 type alias Model =
     { selectedPhotoUrl : Maybe String
+    , photos : Dict String Photo
     }
 
 
@@ -19,12 +32,52 @@ type Msg
 
 initialModel : Model
 initialModel =
-    { selectedPhotoUrl = Nothing }
+    { selectedPhotoUrl = Nothing
+    , photos = Dict.empty
+    }
 
 
 view : Model -> Html Msg
-view _ =
-    Html.div [] [ Html.text "Hello" ]
+view model =
+    let
+        photoByUrl : String -> Maybe Photo
+        photoByUrl url =
+            Dict.get url model.photos
+
+        selectedPhoto : Html Msg
+        selectedPhoto =
+            case Maybe.andThen photoByUrl model.selectedPhotoUrl of
+                Just photo ->
+                    viewSelectedPhoto photo
+
+                Nothing ->
+                    text ""
+    in
+    div [ class "content" ]
+        [ div [ class "selected-photo" ] [ selectedPhoto ] ]
+
+
+viewSelectedPhoto : Photo -> Html Msg
+viewSelectedPhoto photo =
+    div
+        [ class "selected-photo" ]
+        [ h2 [] [ text photo.title ]
+        , img [ src (urlPrefix ++ "photos/" ++ photo.url ++ "/full") ] []
+        , span [] [ text (String.fromInt photo.size ++ "KB") ]
+        , h3 [] [ text "Related" ]
+        , div [ class "related-photos" ]
+            (List.map viewRelatedPhoto photo.relatedUrls)
+        ]
+
+
+viewRelatedPhoto : String -> Html Msg
+viewRelatedPhoto url =
+    img
+        [ class "related-photo"
+        , onClick (ClickedPhoto url)
+        , src (urlPrefix ++ "photos/" ++ url ++ "/thumb")
+        ]
+        []
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -47,7 +100,33 @@ subscriptions _ =
 
 decodeModel : Decoder Model
 decodeModel =
-    Decode.succeed initialModel
+    Decode.succeed
+        { selectedPhotoUrl = Just "trevi"
+        , photos =
+            Dict.fromList
+                [ ( "trevi"
+                  , { title = "Trevi"
+                    , relatedUrls = [ "coli", "fresco" ]
+                    , size = 34
+                    , url = "trevi"
+                    }
+                  )
+                , ( "fresco"
+                  , { title = "Fresco"
+                    , relatedUrls = [ "trevi" ]
+                    , size = 46
+                    , url = "fresco"
+                    }
+                  )
+                , ( "coli"
+                  , { title = "Coliseum"
+                    , relatedUrls = [ "trevi", "fresco" ]
+                    , size = 36
+                    , url = "coli"
+                    }
+                  )
+                ]
+        }
 
 
 init : () -> ( Model, Cmd Msg )
