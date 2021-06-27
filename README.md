f95aa3ad62df97be3f74336e35c85f0dc1f200d8
Eric Stolten <stoltene2@gmail.com>
2021-06-27 15:27:49 -0400

    Added application router to SPA
    - Keep track of active links
    
    - Capture navigations in the app and update address bar or send user
      to the page

--- README.md
+++ README.md
@@ -8,3 +8,27 @@
 
 # Running Main
 `npx elm-live src/Main.elm  --start-page=index.html -- --output app.js`
+
+
+# http-server-spa
+
+This [application](https://www.npmjs.com/package/http-server-spa) is
+used to serve up static assets when we use multiple routes.
+
+`npx http-server-spa .`
+
+
+
+
+# Development setup
+
+## Emacs LSP integration
+
+There is something wrong in the newest version of the elm language
+server 2.1.0 that causes syntax errors. I found [this
+thread](https://github.com/elm-tooling/elm-language-server/issues/598)
+which describes how to fix the problem temporarily.
+
+1. Remove global installation of elm-language server `npm uninstall @elm-tooling/elm-language-server`
+1. Check out elm-language-server e3610b1
+1. Install the checked out version globally `npm install ~/workspace/external/elm-language-server/`
--- src/Main.elm
+++ src/Main.elm
@@ -1,18 +1,24 @@
 module Main exposing (main)
 
 import Browser exposing (Document)
+import Browser.Navigation as Nav
 import Html exposing (Html, a, footer, h1, li, nav, text, ul)
 import Html.Attributes exposing (classList, href)
-import Html.Events exposing (onMouseOver)
 import Html.Lazy exposing (lazy)
+import Maybe as Maybe
+import Url exposing (Url)
+import Url.Parser as Parser exposing ((</>), Parser, s, string)
 
 
 type alias Model =
-    { page : Page }
+    { page : Page
+    , key : Nav.Key
+    }
 
 
 type Page
-    = Gallery
+    = SelectedPhoto String
+    | Gallery
     | Folders
     | NotFound
 
@@ -46,24 +52,63 @@ viewHeader page =
 
         navLink : Page -> { caption : String, url : String } -> Html Msg
         navLink targetPage { caption, url } =
-            li [ classList [ ( "active", page == targetPage ) ] ]
+            li [ classList [ ( "active", isActive { link = targetPage, page = page } ) ] ]
                 [ a [ href url ] [ text caption ] ]
     in
     nav [] [ logo, links ]
 
 
+isActive : { link : Page, page : Page } -> Bool
+isActive { link, page } =
+    case ( link, page ) of
+        ( Gallery, Gallery ) ->
+            True
+
+        ( Gallery, _ ) ->
+            False
+
+        ( Folders, Folders ) ->
+            True
+
+        ( Folders, SelectedPhoto _ ) ->
+            True
+
+        ( Folders, _ ) ->
+            False
+
+        ( SelectedPhoto _, _ ) ->
+            False
+
+        ( NotFound, _ ) ->
+            False
+
+
 viewFooter : Html Msg
 viewFooter =
-    footer [] [ text "This is the footer" ]
+    footer []
+        [ text "This is the footer"
+        , text "and another piece of the footer"
+        ]
 
 
 type Msg
-    = NothingYet
+    = ClickedLink Browser.UrlRequest
+    | ChangeUrl Url
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
-update _ model =
-    ( model, Cmd.none )
+update msg model =
+    case msg of
+        ClickedLink urlRequest ->
+            case urlRequest of
+                Browser.External href ->
+                    ( model, Nav.load href )
+
+                Browser.Internal url ->
+                    ( model, Nav.pushUrl model.key (Url.toString url) )
+
+        ChangeUrl url ->
+            ( { model | page = urlToPage url }, Cmd.none )
 
 
 subscriptions : Model -> Sub Msg
@@ -71,11 +116,33 @@ subscriptions _ =
     Sub.none
 
 
+init : () -> Url -> Nav.Key -> ( Model, Cmd Msg )
+init _ url key =
+    ( { page = urlToPage url, key = key }, Cmd.none )
+
+
+urlToPage : Url -> Page
+urlToPage url =
+    Parser.parse parser url
+        |> Maybe.withDefault NotFound
+
+
+parser : Parser (Page -> a) a
+parser =
+    Parser.oneOf
+        [ Parser.map Folders Parser.top
+        , Parser.map SelectedPhoto (s "photos" </> string)
+        , Parser.map Gallery (s "gallery")
+        ]
+
+
 main : Program () Model Msg
 main =
-    Browser.document
-        { init = \_ -> ( { page = Gallery }, Cmd.none )
+    Browser.application
+        { init = init
         , subscriptions = subscriptions
         , update = update
         , view = view
+        , onUrlRequest = ClickedLink
+        , onUrlChange = ChangeUrl
         }
